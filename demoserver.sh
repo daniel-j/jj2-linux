1886824bdcb0dcf38200d962ff90d319bae651eb
daniel-j <daniel.j.hede@gmail.com>
2016-09-26 09:08:54 +0200

    capture4 is not included, so do battle instead

--- demoserver.sh
+++ demoserver.sh
@@ -2,10 +2,11 @@
 
 path="servers/demo"
 
+# useful flags: -minimize
 # first time the server is run, a new wine prefix will be created. Be patient
 # uncomment next line to run in a virtual X server
 ./xvfb-run \
-./startded "$path" -counts 3 -capture Capture1
+./startded "$path" -maxplayers 32 -counts 10 -battle battle1
 
 # you can join the demo server from another terminal using the main wine prefix:
 # $ ./start game -connect 127.0.0.1
